136c264add22e7e460a3a8fc0641a1cf2906c984
Nina Uspenskaya <nuspenskaya@ibagroup.eu>
2021-11-24 16:59:53 +0300

    remove db

--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,14 @@ Rails.application.routes.draw do
       # services
       get '/services', to: 'services#index'
       get '/services/:service_name', to: 'services#show', constraints: { :service_name => /[0-z\.]+/ }
-      get '/services/:service_name/overview', to: 'subscriptions#show', constraints: { :service_name => /[0-z\.]+/ } # this is correct path
+      get '/services/:service_name/overview', to: 'services#overview', constraints: { :service_name => /[0-z\.]+/ } # this is correct path
+      post '/services/:service_name', to: 'services#create', constraints: { :service_name => /[0-z\.]+/ }
 
       # versions
       get '/services/:service_name/version', to: 'versions#show', constraints: { :service_name => /[0-z\.]+/ }
       put '/services/:service_name/version', to: 'versions#create', constraints: { :service_name => /[0-z\.]+/ }
-      get '/services/:service_name/versions', to: 'versions#downgrade_versions', constraints: { :service_name => /[0-z\.]+/ }
+      get '/services/:service_name/installed_versions', to: 'versions#downgrade_versions', constraints: { :service_name => /[0-z\.]+/ }
+      get '/services/:service_name/service_versions', to: 'versions#installed_github_versions', constraints: { :service_name => /[0-z\.]+/ }
 
       #updates
       put '/services/:service_name/update', to: 'updates#create', constraints: { :service_name => /[0-z\.]+/ }
