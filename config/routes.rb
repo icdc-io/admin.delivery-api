e8a57c88c93f0f6bb751694ad6b45613f8e5daa8
Uladzislau Halukha <UHalukha@ibagroup.eu>
2022-02-19 13:38:39 +0300

    fix spaces

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
       get '/services/:service_name/overview', to: 'services#overview', constraints: { :service_name => /[0-z\.]+/ }
       post '/services/:service_name', to: 'services#create', constraints: { :service_name => /[0-z\.]+/ }
       put '/services/:service_name', to: 'services#upgrade', constraints: { :service_name => /[0-z\.]+/ }
-      delete '/services/:service_name', to: 'services#delete', constraints: { :service_name => /[0-z\.]+/ } 
+      delete '/services/:service_name', to: 'services#delete', constraints: { :service_name => /[0-z\.]+/ }
 
       # versions
       get '/services/:service_name/version', to: 'versions#show', constraints: { :service_name => /[0-z\.]+/ }
@@ -20,11 +20,11 @@ Rails.application.routes.draw do
       get '/services/:service_name/update', to: 'updates#show', constraints: { :service_name => /[0-z\.]+/ }
 
       # releases
-      put '/services/:service_name/release', to: 'releases#create', constraints: { :service_name => /[0-z\.]+/ } 
+      put '/services/:service_name/release', to: 'releases#create', constraints: { :service_name => /[0-z\.]+/ }
       get '/services/:service_name/release', to: 'releases#show', constraints: { :service_name => /[0-z\.]+/ }
 
       # status
-      get '/services/:service_name/status', to: 'statuses#show', constraints: { :service_name => /[0-z\.]+/ } 
+      get '/services/:service_name/status', to: 'statuses#show', constraints: { :service_name => /[0-z\.]+/ }
     end
   end
 
