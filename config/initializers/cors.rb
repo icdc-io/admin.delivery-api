d35cb06662185752057c26a0a9e234aeb17b5d5c
Danat Memekh <dmemekh@ibagroup.eu>
2023-03-15 12:23:59 +0000

    Update cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -14,4 +14,13 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
+
+  allow do
+    origins 'https://code.zby.scdc.io'
+    #origins ENV.fetch('CORS_ORIGINS').split(" ").map(&:strip)
+
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+  end
 end
