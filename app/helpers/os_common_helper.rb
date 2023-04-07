ba462dfdad431495c46c8f0cb50c7cdc84ab392c
Danat Memekh <dmemekh@ibagroup.eu>
2023-04-07 11:00:32 +0000

    Update os_common_helper.rb

--- app/helpers/os_common_helper.rb
+++ app/helpers/os_common_helper.rb
@@ -16,6 +16,11 @@ module OsCommonHelper
 #    nmsp
 #  end
 
+  def service_config
+    file = YAML.load_file("config/services.yaml")
+    JSON.parse(file.to_json.to_s.gsub("$","#{ENV["NAMESPACE_PREFIX"].present? ? ENV["NAMESPACE_PREFIX"] : "cloud"}"))
+  end
+
   def get_os_namespace(service_name)
     namespace_prefix = "#{ENV["NAMESPACE_PREFIX"].present? ? ENV["NAMESPACE_PREFIX"] : "cloud"}".downcase
     namespace_prefix + "-" + service_name
