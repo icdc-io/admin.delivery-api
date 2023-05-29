385da20db26f2865d12492b2a147d6c53bb4510f
Danat Memekh <dmemekh@ibagroup.eu>
2023-05-29 12:47:22 +0000

    update dns settings

--- app/helpers/service_helper.rb
+++ app/helpers/service_helper.rb
@@ -2,8 +2,8 @@ module ServiceHelper
     include OsHelper
     def create_dns_record(hostname, dns_host)
       location_domain = "#{ENV["LOCATION_DOMAIN"]}"
-      account = ENV["SYS_ACCOUNT"] || "sys"
-      dns_ttl = ENV["DNS_TTL"].to_i || 600
+      account = ENV["DNS_ACCOUNT"] || "sys"
+      dns_ttl = ENV["DNS_TTL"].to_i || 3600
       dns_owner = ENV["DNS_OWNER"] || "admin@#{location_domain}"
       puts "---CREATE FQDN---"
       puts "ttl"=>dns_ttl,"metadata"=>{"account"=> account,"owner"=>dns_owner}, "group"=>"#{hostname}.#{location_domain}","host"=>Resolv.getaddress(dns_host)
