c4774e2a6dbe0d2abcd158419a42c16a1bb469ff
Danat Memekh <dmemekh@ibagroup.eu>
2023-05-29 10:12:01 +0000

    Update dns settings

--- app/helpers/service_helper.rb
+++ app/helpers/service_helper.rb
@@ -1,19 +1,22 @@
 module ServiceHelper
     include OsHelper
-    def create_dns_record(service_name)
-        location_platform_io = "#{ENV["LOCATION_DOMAIN"]}"
+    def create_dns_record(service_name)                                                               
+        location_domain = "#{ENV["LOCATION_DOMAIN"]}"
         account = ENV["SYS_ACCOUNT"] || "sys"
+        dns_ttl = ENV["DNS_TTL"].to_i || 600
+        dns_owner = ENV["DNS_OWNER"] || "admin@#{location_domain}"
         puts "---CREATE FQDN---"
-        puts "ttl"=>3600,"metadata"=>{"account"=> account}, "group"=>"#{service_name}.#{location_platform_io}","host"=>Resolv.getaddress("api.#{location_platform_io}")
-        coredns.domain("#{service_name}.#{location_platform_io}").add("ttl"=>3600,"metadata"=>{"account"=> account}, "group"=>"#{service_name}.#{location_platform_io}","host"=>Resolv.getaddress("api.#{location_platform_io}"))
-    end
+        puts "ttl"=>dns_ttl,"metadata"=>{"account"=> account,"owner"=>dns_owner}, "group"=>"#{hostname}.#{location_domain}","host"=>Resolv.getaddress(dns_host)                                                            
+        coredns.domain("#{service_name}.#{location_domain}").add("ttl"=>dns_ttl,"metadata"=>{"account"=> account,"owner"=>dns_owner}, "group"=>"#{hostname}.#{location_domain}","host"=>Resolv.getaddress(dns_host))       
+    end                                                                      
+
 
     def delete_dns_record(service_name)
         puts "---DELETE FQDN---"
-        location_platform_io = "#{ENV["LOCATION_DOMAIN"]}"
-        domain = coredns.domain("#{service_name}.#{location_platform_io}")
+        location_domain = "#{ENV["LOCATION_DOMAIN"]}"
+        domain = coredns.domain("#{hostname}.#{location_domain}")
         domain.list_all.each{|record| domain.delete("name"=> record["name"])}
-    end
+    end                                     
 
     def coredns
         CoreDns::Etcd.new(ENV['DNS_SERVER'])
