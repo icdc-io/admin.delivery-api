80cdaa6179dc7d9684a4c1390c563c840d3d32b3
Danat Memekh <dmemekh@ibagroup.eu>
2023-08-07 11:12:30 +0000

    enable debug log level

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -55,5 +55,6 @@ Rails.application.configure do
   # config.action_cable.disable_request_forgery_protection = true
   config.hosts.clear
   
-  config.log_level = :warn
+  #config.log_level = :warn
+  config.log_level = :debug
 end
