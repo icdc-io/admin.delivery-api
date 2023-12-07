68abc8f9ae784b8ec4f004b357dc412c7015aa50
Aliaksei Hrechushkin <ahrechushkin@gmail.com>
2023-12-07 12:09:27 +0300

    fetch service_repository not from env

--- app/helpers/os_helper.rb
+++ app/helpers/os_helper.rb
@@ -194,7 +194,7 @@ module OsHelper
   def update_service(service_name, required_service)
     #service_repository = get_service_repository(service_name)["parameters"].map{ |param| param["value"] if param["name"] == "SERVICE_REPOSITORY"}.compact.first
     namespace = get_os_namespace(service_name)
-    registry_server =  get_service_repository(service_name)["parameters"].find {|x| x["name"] == "REGISTRY_SERVER" }["value"]
+    registry_server =  ENV['REGISTRY_SERVER'] || get_service_repository(service_name)["parameters"].find {|x| x["name"] == "REGISTRY_SERVER" }["value"]
     service_repository = "#{registry_server}/#{namespace}"
     applications = required_service["applications"].compact.map do |app|
       create_image_stream_tag(app['name'], app["tag"], service_repository, service_name)
