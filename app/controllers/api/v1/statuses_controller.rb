c0c840137aeb688ef2e65d430020864fd1efdfba
ahrechushkin@ibagroup.eu <ahrechushkin@ibagroup.eu>
2025-05-20 10:41:32 +0000

    rename mock data filename

--- app/controllers/api/v1/statuses_controller.rb
+++ app/controllers/api/v1/statuses_controller.rb
@@ -6,7 +6,7 @@ class Api::V1::StatusesController < ApplicationController
   before_action :setup_prefix, only: [:apps, :show]
 
   def apps
-    mock_data = Rails.root.join(ENV.fetch('MOCKDATA_FILENAME', 'config/extra/apps.json'))
+    mock_data = Rails.root.join(ENV.fetch('MOCKDATA_FILENAME', 'config/extra/apps.dev.json'))
     if File.exist?(mock_data)
       resp = JSON.parse(File.read(mock_data)).map(&:deep_symbolize_keys)
     else
